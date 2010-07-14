2cc409001e2fc39cd91e8b5e438c7490ebb0a9b8
Egor Lynko <flexoid@gmail.com>
2010-07-15 00:50:18 +0300

    Added something resembling a graphical interface Updated ru_RU translation Added some useful methods in MainWindow

--- src/translations/ru_RU.ts
+++ src/translations/ru_RU.ts
@@ -3,6 +3,87 @@
 <TS version="2.0" language="ru_RU">
 <defaultcodec>UTF-8</defaultcodec>
 <context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="85"/>
+        <source>Go to alarm date</source>
+        <translation>Перейти к сигналу</translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="115"/>
+        <source>&amp;Refresh</source>
+        <translation>&amp;Обновить</translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="56"/>
+        <source>dd.MM.yyyy HH:mm</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="125"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Настройки</translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="148"/>
+        <source>Set &amp;alarm time</source>
+        <translation>&amp;Установить время сигнала</translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="105"/>
+        <source>Go to today</source>
+        <translation>Перейти на сегодня</translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="190"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="157"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="169"/>
+        <location filename="../gui/ui/mainwindow.ui" line="176"/>
+        <location filename="../gui/ui/mainwindow.ui" line="197"/>
+        <source>CheckBox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="183"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alarm is set</source>
+        <translation type="obsolete">Сигнал установлен</translation>
+    </message>
+    <message>
+        <source>Alarm isn&apos;t set</source>
+        <translation type="obsolete">Сигнал не установлен</translation>
+    </message>
+    <message>
+        <source>$1 Alarm is set $2</source>
+        <translation type="obsolete">$1 Сигнал установлен $2</translation>
+    </message>
+    <message>
+        <source>$1 Alarm isn&apos;t set $2</source>
+        <translation type="obsolete">$1 Сигнал не установлен $2</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.cpp" line="84"/>
+        <source>%1 Alarm is set %2</source>
+        <translation>%1 Сигнал установлен %2</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.cpp" line="89"/>
+        <source>%1 Alarm isn&apos;t set %2</source>
+        <translation>%1 Сигнал не установлен %2</translation>
+    </message>
+</context>
+<context>
     <name>Rtc</name>
     <message>
         <location filename="../core/rtc.cpp" line="54"/>
