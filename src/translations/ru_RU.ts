ec4d9c67c502ff17885548ad2840535265c30318
Egor Lynko <flexoid@gmail.com>
2010-07-17 00:08:11 +0300

    ru_RU updated

--- src/translations/ru_RU.ts
+++ src/translations/ru_RU.ts
@@ -5,10 +5,6 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <source>Go to alarm date</source>
-        <translation type="obsolete">Перейти к сигналу</translation>
-    </message>
-    <message>
         <location filename="../gui/ui/mainwindow.ui" line="115"/>
         <source>&amp;Refresh</source>
         <translation>&amp;Обновить</translation>
@@ -29,64 +25,59 @@
         <translation>Перейти к текущему месяцу</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="125"/>
+        <location filename="../gui/ui/mainwindow.ui" line="147"/>
+        <source>Show log</source>
+        <translation>Показать лог</translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="160"/>
         <source>&amp;Settings</source>
         <translation>&amp;Настройки</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="148"/>
+        <location filename="../gui/ui/mainwindow.ui" line="138"/>
         <source>Set &amp;alarm time</source>
         <translation>&amp;Установить время сигнала</translation>
     </message>
     <message>
-        <source>Go to today</source>
-        <translation type="obsolete">Перейти на сегодня</translation>
-    </message>
-    <message>
-        <location filename="../gui/ui/mainwindow.ui" line="190"/>
+        <location filename="../gui/ui/mainwindow.ui" line="207"/>
         <source>PushButton</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="157"/>
+        <location filename="../gui/ui/mainwindow.ui" line="174"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="169"/>
-        <location filename="../gui/ui/mainwindow.ui" line="176"/>
-        <location filename="../gui/ui/mainwindow.ui" line="197"/>
+        <location filename="../gui/ui/mainwindow.ui" line="224"/>
+        <source>Log</source>
+        <translation>Лог</translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="230"/>
+        <source>Clean</source>
+        <translation>Очистить</translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="186"/>
+        <location filename="../gui/ui/mainwindow.ui" line="193"/>
+        <location filename="../gui/ui/mainwindow.ui" line="214"/>
         <source>CheckBox</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="183"/>
+        <location filename="../gui/ui/mainwindow.ui" line="200"/>
         <source>...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Alarm is set</source>
-        <translation type="obsolete">Сигнал установлен</translation>
-    </message>
-    <message>
-        <source>Alarm isn&apos;t set</source>
-        <translation type="obsolete">Сигнал не установлен</translation>
-    </message>
-    <message>
-        <source>$1 Alarm is set $2</source>
-        <translation type="obsolete">$1 Сигнал установлен $2</translation>
-    </message>
-    <message>
-        <source>$1 Alarm isn&apos;t set $2</source>
-        <translation type="obsolete">$1 Сигнал не установлен $2</translation>
-    </message>
-    <message>
-        <location filename="../gui/mainwindow.cpp" line="84"/>
+        <location filename="../gui/mainwindow.cpp" line="96"/>
         <source>%1 Alarm is set %2</source>
         <translation>%1 Сигнал установлен %2</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.cpp" line="89"/>
+        <location filename="../gui/mainwindow.cpp" line="101"/>
         <source>%1 Alarm isn&apos;t set %2</source>
         <translation>%1 Сигнал не установлен %2</translation>
     </message>
@@ -95,7 +86,7 @@
     <name>Rtc</name>
     <message>
         <location filename="../core/rtc.cpp" line="54"/>
-        <location filename="../core/rtc.cpp" line="109"/>
+        <location filename="../core/rtc.cpp" line="115"/>
         <source>File %1 doesn&apos;t exist.</source>
         <translation>Файл %1 не создан.</translation>
     </message>
@@ -115,12 +106,17 @@
         <translation>Неправильный формат времени.</translation>
     </message>
     <message>
-        <location filename="../core/rtc.cpp" line="116"/>
+        <location filename="../core/rtc.cpp" line="107"/>
+        <source>The specified time is in the past.</source>
+        <translation>Задано время в прошлом.</translation>
+    </message>
+    <message>
+        <location filename="../core/rtc.cpp" line="122"/>
         <source>Don&apos;t have permission to write %1 file.</source>
         <translation>Недостаточно прав для записи в файл %1.</translation>
     </message>
     <message>
-        <location filename="../core/rtc.cpp" line="127"/>
+        <location filename="../core/rtc.cpp" line="133"/>
         <source>New time isn&apos;t written in %1.</source>
         <translation>Новаое время не было записано в %1.</translation>
     </message>
