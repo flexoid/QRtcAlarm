d6451aef94f8dfaffd948f047a0cc2d5c838e817
Egor Lynko <flexoid@gmail.com>
2010-07-22 22:28:19 +0300

    Translation files no longer contain the <location> lines

--- src/translations/ru_RU.ts
+++ src/translations/ru_RU.ts
@@ -5,68 +5,54 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="89"/>
         <source>Go to alarm month</source>
         <translation>Перейти к месяцу сигнала</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="109"/>
         <source>Go to current month</source>
         <translation>Перейти к текущему месяцу</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="157"/>
-        <location filename="../gui/ui/mainwindow.ui" line="185"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="119"/>
         <source>Refresh</source>
         <translation>Обновить</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="56"/>
         <source>yyyy-MM-dd hh:mm</source>
         <translation>dd.MM.yyyy hh:mm</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="144"/>
         <source>Log window</source>
         <translation>Окно лога</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="176"/>
         <source>Set alarm time</source>
         <translation>Установить время сигнала</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="249"/>
         <source>Log</source>
         <translation>Лог</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="255"/>
         <source>Clean</source>
         <translation>Очистить</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="169"/>
         <source>Reset alarm</source>
         <translation>Сбросить сигнал</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.cpp" line="83"/>
         <source>For translators: just enter number of the first day of the week in the field of translation, where 1 is Monday and 7 is Sunday</source>
         <translation>1</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.cpp" line="119"/>
         <source>%1 Alarm is set %2</source>
         <translation>%1 Сигнал установлен %2</translation>
     </message>
     <message>
-        <location filename="../gui/mainwindow.cpp" line="129"/>
         <source>%1 Alarm isn&apos;t set %2</source>
         <translation>%1 Сигнал не установлен %2</translation>
     </message>
@@ -74,40 +60,30 @@
 <context>
     <name>Rtc</name>
     <message>
-        <location filename="../core/rtc.cpp" line="55"/>
-        <location filename="../core/rtc.cpp" line="116"/>
-        <location filename="../core/rtc.cpp" line="164"/>
         <source>File %1 doesn&apos;t exist.</source>
         <translation>Файл %1 не существует.</translation>
     </message>
     <message>
-        <location filename="../core/rtc.cpp" line="62"/>
-        <location filename="../core/rtc.cpp" line="171"/>
         <source>Don&apos;t have permission to read %1 file.</source>
         <translation>Недостаточно прав для чтения файла %1.</translation>
     </message>
     <message>
-        <location filename="../core/rtc.cpp" line="72"/>
         <source>Alarm is not set or it is incorrect.</source>
         <translation>Сигнал не установлен или установлен неверно.</translation>
     </message>
     <message>
-        <location filename="../core/rtc.cpp" line="89"/>
         <source>Invalid time spec.</source>
         <translation>Неправильный формат времени.</translation>
     </message>
     <message>
-        <location filename="../core/rtc.cpp" line="108"/>
         <source>The specified time is in the past.</source>
         <translation>Задано время в прошлом.</translation>
     </message>
     <message>
-        <location filename="../core/rtc.cpp" line="123"/>
         <source>Don&apos;t have permission to write %1 file.</source>
         <translation>Недостаточно прав для записи в файл %1.</translation>
     </message>
     <message>
-        <location filename="../core/rtc.cpp" line="141"/>
         <source>New time isn&apos;t written in %1.</source>
         <translation>Новаое время не было записано в %1.</translation>
     </message>
